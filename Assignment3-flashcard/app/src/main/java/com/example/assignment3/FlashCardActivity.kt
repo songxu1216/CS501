962636e23f6d25d52e59a59e3938768d2b52da21
songxu1216 <kimir7@icloud.com>
2023-02-12 16:38:28 -0500

    Update FlashCardActivity.kt

--- Assignment3-flashcard/app/src/main/java/com/example/assignment3/FlashCardActivity.kt
+++ Assignment3-flashcard/app/src/main/java/com/example/assignment3/FlashCardActivity.kt
@@ -1,9 +1,12 @@
 package com.example.assignment3
 
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import android.support.v7.app.AppCompatActivity
 import android.widget.*
 import kotlin.random.Random
+
 
 class FlashCardActivity : AppCompatActivity() {
     private lateinit var generateButton: Button
@@ -11,16 +14,24 @@ class FlashCardActivity : AppCompatActivity() {
     private lateinit var problemTextView: TextView
     private lateinit var answerEditText: EditText
 
-    private var problems = mutableListOf<Problem>()
+    private var problems = arrayListOf<Problem>()
     private var currentProblemIndex = 0
     private var score = 0
     private var isGenerated = false
+
+    companion object {
+        const val KEY_CURRENT_PROBLEM_INDEX = "current_problem_index"
+        const val KEY_SCORE = "score"
+        const val KEY_IS_GENERATED = "is_generated"
+        const val KEY_PROBLEMS = "problems"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flash_card)
 
         val username = intent.getStringExtra("username")
+        Toast.makeText(this, "Welcome $username", Toast.LENGTH_SHORT).show()
 
         generateButton = findViewById(R.id.generateProblemsButton)
         submitButton = findViewById(R.id.submitAnswerButton)
@@ -58,10 +69,32 @@ class FlashCardActivity : AppCompatActivity() {
                 Toast.makeText(this, "Score: $score out of ${problems.size}", Toast.LENGTH_SHORT).show()
             }
         }
+
+        if (savedInstanceState != null) {
+            currentProblemIndex = savedInstanceState.getInt(KEY_CURRENT_PROBLEM_INDEX)
+            score = savedInstanceState.getInt(KEY_SCORE)
+            isGenerated = savedInstanceState.getBoolean(KEY_IS_GENERATED)
+            problems = savedInstanceState.getParcelableArrayList<Problem>(KEY_PROBLEMS) ?: arrayListOf()
+
+
+            if (isGenerated) {
+                generateButton.isEnabled = false
+                submitButton.isEnabled = true
+                showProblem()
+            }
+        }
     }
 
-    private fun generateProblems(): MutableList<Problem> {
-        val problems = mutableListOf<Problem>()
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY_CURRENT_PROBLEM_INDEX, currentProblemIndex)
+        outState.putInt(KEY_SCORE, score)
+        outState.putBoolean(KEY_IS_GENERATED, isGenerated)
+        outState.putParcelableArrayList(KEY_PROBLEMS, ArrayList(problems))
+    }
+
+    private fun generateProblems(): ArrayList<Problem> {
+        val problems = ArrayList<Problem>()
 
         for (i in 1..10) {
             val operator = if (Random.nextBoolean()) "+" else "-"
@@ -81,9 +114,36 @@ class FlashCardActivity : AppCompatActivity() {
         answerEditText.text.clear()
     }
 
-    data class Problem(val operand1: Int, val operand2: Int, val operator: String) {
+    data class Problem(val operand1: Int, val operand2: Int, val operator: String) : Parcelable {
         val answer: Int
             get() = if (operator == "+") operand1 + operand2 else operand1 - operand2
+
+        constructor(parcel: Parcel) : this(
+            parcel.readInt(),
+            parcel.readInt(),
+            parcel.readString()!!
+        )
+
+        override fun writeToParcel(parcel: Parcel, flags: Int) {
+            parcel.writeInt(operand1)
+            parcel.writeInt(operand2)
+            parcel.writeString(operator)
+        }
+
+        override fun describeContents(): Int {
+            return 0
+        }
+
+        companion object CREATOR : Parcelable.Creator<Problem> {
+            override fun createFromParcel(parcel: Parcel): Problem {
+                return Problem(parcel)
+            }
+
+            override fun newArray(size: Int): Array<Problem?> {
+                return arrayOfNulls(size)
+            }
+        }
     }
+
 }
 
